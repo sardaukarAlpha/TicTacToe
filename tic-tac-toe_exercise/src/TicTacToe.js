3cdeb91821aac532479969c5bdc95d9968263eb8
Saul Sosa <saul.sosa@outlook.com>
2022-01-24 15:19:06 -0600

    created first test

---
+++ tic-tac-toe_exercise/src/TicTacToe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TicTacToe(){
+    return(
+    <h2>TicTacToe game</h2>
+    )
+}
---
+++ tic-tac-toe_exercise/src/__tests__/TicTacToe.test.js
@@ -0,0 +1,10 @@
+import TicTacToe from "../TicTacToe";
+import { shallow } from "enzyme";
+
+describe("TicTacToe Testing", () => {
+    test("render the title of the game", () => {
+        const wrapper = shallow(<TicTacToe />);
+        expect(wrapper.find('h2').text()).toBe('TicTacToe game');
+    })
+})
+
