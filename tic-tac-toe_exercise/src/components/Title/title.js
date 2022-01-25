a9a277d930dffdb75225d0f609bf52302c784e39
Saul Sosa <saul.sosa@outlook.com>
2022-01-24 20:14:49 -0600

    added test case to validate if title component renders title prop as expected

---
+++ tic-tac-toe_exercise/src/components/Title/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Title(props){
+    const { title } = props;
+    return(
+    <h2>{ title }</h2>
+    )
+}
+
+export default Title;
---
+++ tic-tac-toe_exercise/src/components/Title/title.test.js
@@ -0,0 +1,13 @@
+import Title from "../../components/Title/title";
+import { shallow, mount } from "enzyme";
+
+describe("title testing", () => {
+    test('renders title component without crashing', () => {
+        shallow(<Title />);
+    });
+    test('renders a button with text of restart', () => {
+        const titleText = "Tic tac toe game, let's play!";
+        const wrapper = mount(<Title title={titleText} />);
+        expect(wrapper.prop('title')).toBe("Tic tac toe game, let's play!");
+    });
+})
