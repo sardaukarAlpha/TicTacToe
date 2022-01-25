e6abfe0894420ecac580e0d46c0b46bf57ffa41d
Saul Sosa <saul.sosa@outlook.com>
2022-01-25 00:47:39 -0600

    passed test case to validate square's value should have x value when it is X

--- tic-tac-toe_exercise/src/__tests__/TicTacToe.test.js
+++ tic-tac-toe_exercise/src/__tests__/TicTacToe.test.js
@@ -1,9 +1,14 @@
 import TicTacToe from "../TicTacToe";
+import TicTacToeSquare from "../components/TicTacToeSquare/ticTacToeSquare";
 import { shallow, mount } from "enzyme";
 
 describe("TicTacToe Testing", () => {
     test('renders TicTacToe component without crashing', () => {
         shallow(<TicTacToe />);
+    });
+    test("should have value as X when it is X ", () => {
+        const squareX = shallow(<TicTacToeSquare value="X" />);
+        expect(squareX.text()).toEqual("X");
     });
 })
 
