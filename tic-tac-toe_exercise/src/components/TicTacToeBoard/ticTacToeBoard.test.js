e2f9db9a528012f67c731cc8b9c4c558d3fe1a68
Saul Sosa <saul.sosa@outlook.com>
2022-01-25 09:17:45 -0600

    created test case to validate if board component is loaded without crashing

--- tic-tac-toe_exercise/src/components/TicTacToeBoard/ticTacToeBoard.test.js
+++ tic-tac-toe_exercise/src/components/TicTacToeBoard/ticTacToeBoard.test.js
@@ -2,6 +2,12 @@ import TicTacToeBoard from "../../components/TicTacToeBoard/ticTacToeBoard";
 import { shallow } from "enzyme";
 
 describe("TicTacToe board Testing", () => {
+    test('validate if TicTacToeBoard component is executed without crashing', () => {
+        const props = {
+            onClick: jest.fn()
+        }
+        shallow(<TicTacToeBoard {...props} />);
+    });
     test('if function passed to TicTacToBoard component is executed', () => {
         const onClick = jest.fn();
         // eslint-disable-next-line no-unused-vars
