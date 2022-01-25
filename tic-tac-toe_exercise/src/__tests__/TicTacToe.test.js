4c2e8195d3d261f0e1ee66cb5f1215f8a596edaa
Saul Sosa <saul.sosa@outlook.com>
2022-01-25 15:48:19 -0600

    Added test case to validate the board has 3 rows

--- tic-tac-toe_exercise/src/__tests__/TicTacToe.test.js
+++ tic-tac-toe_exercise/src/__tests__/TicTacToe.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TicTacToe from "../TicTacToe";
 import TicTacToeSquare from "../components/TicTacToeSquare/ticTacToeSquare";
+import TicTacToeBoard from "../components/TicTacToeBoard/ticTacToeBoard";
 import { shallow } from "enzyme";
 
 describe("TicTacToe Testing", () => {
@@ -21,7 +22,21 @@ describe("TicTacToe Testing", () => {
     test("should have value as O when it is O", () => {
         const squareO = shallow(<TicTacToeSquare value="O" />);
         expect(squareO.text()).toEqual("O");
+    });    
+    it("the board should contain 3 rows", () => {
+        const wrapper = shallow(<TicTacToeBoard />);
+        expect(wrapper.find(".squareRow").length).toEqual(3);
     });
+    
+    // it("should render all the slots for the board", () => {
+    //     expect(board.find(".squareinit").exists()).toBeTruthy();
+    // });
+    
+    // it("should have nine squares", () => {
+    //     const board = mount(<Board squares={Array(9).fill(null)} />);
+    
+    //     expect(board.find("#squareDiv").length).toEqual(9);
+    // });
     // test("initializes the square state with null", () => {
     //     jest.spyOn(React, 'useState').mockImplementation(useStateMock);
     //     // eslint-disable-next-line no-unused-vars
